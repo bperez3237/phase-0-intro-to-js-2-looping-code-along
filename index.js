0979864417c58534d28a09cf51fdb12485a42191
Brian <b.perez3237@gmail.com>
2022-01-20 20:31:54 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names,eventName) {
+    let cards = [];
+    for (let i=0; i<names.length; i+=1) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    }
+    return cards;
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -=1
+    }
+    console.log(num);
+
+}
